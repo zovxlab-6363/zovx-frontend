c0a654f6a9ba61b9de58b01152727adf15418c1a
pavan3030-vikal <zovxlab@gmail.com>
2025-08-16 14:37:33 +0530

    navbar neww
    navbar neww

--- zovx/src/components/NeonNavigation.tsx
+++ zovx/src/components/NeonNavigation.tsx
@@ -6,23 +6,23 @@ export default function NeonNavigation() {
     <nav className={styles.nav}>
       <ul className={styles.navList}>
         <li className={styles.navItem} style={{ "--clr": "#00ade1" } as React.CSSProperties}>
-          <Link href="/" className={styles.navLink} data-text="&nbsp;Home">
-            &nbsp;Home&nbsp;
+          <Link href="/" className={styles.navLink}>
+            Home
           </Link>
         </li>
         <li className={styles.navItem} style={{ "--clr": "#ffdd1c" } as React.CSSProperties}>
-          <Link href="/category/ecommerce-brands" className={styles.navLink} data-text="&nbsp;E-commerce">
-            &nbsp;E-commerce&nbsp;
+          <Link href="/category/ecommerce-brands" className={styles.navLink}>
+            E-commerce
           </Link>
         </li>
         <li className={styles.navItem} style={{ "--clr": "#00dc82" } as React.CSSProperties}>
-          <Link href="/category/content-creators" className={styles.navLink} data-text="&nbsp;Content Creators">
-            &nbsp; Content Creators&nbsp;
+          <Link href="/category/content-creators" className={styles.navLink}>
+            Creators
           </Link>
         </li>
         <li className={styles.navItem} style={{ "--clr": "#9b59b6" } as React.CSSProperties}>
-          <Link href="/contact" className={styles.navLink} data-text="&nbsp;Custom AI">
-            &nbsp;Custom AI&nbsp;
+          <Link href="/contact" className={styles.navLink}>
+            Custom AI
           </Link>
         </li>
       </ul>
